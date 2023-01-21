06eef56d546cf1bd1b7086f61d5319c1d3dd53ca
Danny Brown <xaerr00@gmail.com>
2023-01-20 23:51:43 -0800

    Customer Website

---
+++ src/main/java/com/springboot/customerwebsite/CustomerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.customerwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/customerwebsite/CustomerWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.customerwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
