dcf303888de74fd3ffad135a5aeb73a5d6ee0c66
Danny Brown <xaerr00@gmail.com>
2023-03-10 13:59:09 -0800

    Cleaned code

--- src/main/java/com/springboot/customerwebsite/repositories/AuthorityRepo.java
+++ src/main/java/com/springboot/customerwebsite/repositories/AuthorityRepo.java
@@ -5,10 +5,7 @@ import com.springboot.customerwebsite.models.securitymodels.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AuthorityRepo extends JpaRepository<Authority, Long> {
     Authority findRoleById(Long id);
-//    Authority findByMultipleId(List<Long> idList);
 }
